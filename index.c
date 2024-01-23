70971cbfbf424a8c5041ec90dbba197b7e05762e
Patel-love <manavamrutiya29@gmail.com>
2024-01-23 12:00:57 +0530

    function

---
+++ index.c
@@ -0,0 +1,47 @@
+// ------------Q-1-------------
+// #include <stdio.h>
+// float average(float a[], int n);
+// int main() {
+//     float a[]={6,7,8,9,5};
+//     int n = sizeof(a) / sizeof(a[0]);
+//     float avg = average(a, n);
+//     printf("The average of  %.2f", avg);
+//     return 0;
+// }
+// float average(float a[], int n) {
+//     int i;
+//     float sum = 0.0;
+//     for (i = 0; i < n; i++) {
+//         sum += a[i];
+//     }
+//     return sum / n;
+// }
+
+
+
+// ------------Q-2-------------
+// #include <stdio.h>
+
+// void number(int m) {
+//     if (m>0) {
+//        number(m-1);
+//         printf("%d\n",m);
+//     }
+// }
+// int main() {
+//     number(10);
+//     return 0;
+// }
+
+// ------------Q-3-------------
+// #include <stdio.h>
+// #include <string.h>
+
+// int main()
+// {
+//     char lowercase[]="red";
+//     printf("enter your lowercase ");
+//     scanf("%s",&lowercase);
+//     printf("%s",strupr(lowercase));
+//     return 0;
+// }
